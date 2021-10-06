c023b6ec96749cfa87c67bfbcdbee8433bfdbcc6
Localt <85951918+Localtings@users.noreply.github.com>
2021-10-06 11:38:35 +0700

    Add files via upload

---
+++ RoTracker/trigger.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    var status;
+    var onlineMessage;
+    var offlineMessage;
+    var loadAPIFailMessage;
+
+    $.ajax({
+        url: "textDisplay.json",
+        dataType: "json",
+        type: 'get',
+        cache: false,
+        success: function(data) {
+            onlineMessage = data.Online;
+            offlineMessage = data.Offline;
+            loadAPIFailMessage = data.ErrorAPI;
+        }
+    });
+
+
+    function displaySuccess(data, id) {
+        if (data.IsOnline == true) {
+            status = onlineMessage;
+        } else {
+            status = offlineMessage + result.LastOnline.substr(0, 10);
+        }
+        $(id).text(status);  
+    }
+    function displayFail(id) {
+        $(id).text(loadAPIFailMessage);
+    }
+
+    $("#btn").click(function(){  
+        var rblx_id = document.getElementById("rblx_id").value;
+        
+        try {
+            $.ajax({
+                url: "https://api.roblox.com/users/"+rblx_id+"/onlinestatus/",
+                dataType: "json",
+                type: 'get',
+                cache: false,
+                success: function(data) {
+                    displaySuccess(data, "#text");
+                }
+            });
+        } catch(error) {
+            displayFail("#text");
+        }
+    });
+});
